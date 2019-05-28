c0fd72313614462e2a53fb2c4736216740d29ccf
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-05-28 13:03:57 +0200

    Add config.js with google auth

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID:
+    "421850548520-50l00pri3hhnen7m1r40skhoaug0h0j9.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "ajgRPabjptefXlGcXHJQynbd",
+  CALLBACK_URL: "http://localhost:3000/auth/google/callback"
+};
