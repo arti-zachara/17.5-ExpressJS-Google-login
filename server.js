c332ce54ce2678a2e1a478903247c3d80950caa1
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-05-28 14:09:15 +0200

    Add pug and google auth via passport

--- server.js
+++ server.js
@@ -1,32 +1,72 @@
 var express = require("express");
+var passport = require("passport"); // middleware
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+var config = require("./config");
+// ------------- requirements
+
 var app = express();
+var googleProfile = {};
 
 // ----- Pug
 app.set("view engine", "pug");
 app.set("views", "./views");
+// ----- Passport
+app.use(passport.initialize());
+app.use(passport.session());
+// ----- css styles
+app.use(express.static("css"));
 
-app.use(express.static("css")); //is it really necessary?
+// ----- serialize and deserialize user
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
-// ----- GET: main page (pug)
+// ----- passport: instance of Google strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.GOOGLE_CLIENT_ID,
+      clientSecret: config.GOOGLE_CLIENT_SECRET,
+      callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+      };
+      cb(null, profile);
+    }
+  )
+);
+
+// ----- app routes
 app.get("/", function(req, res) {
-  res.render("index.pug");
+  res.render("index.pug", { user: req.user });
+});
+app.get("/logged", function(req, res) {
+  res.render("logged.pug", { user: googleProfile });
+  console.log(googleProfile);
 });
 
-// ----- GET: login (pug)
-app.get("/login", function(req, res) {
-  res.render("login.pug");
-});
+// ----- Passport routes
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/logged",
+    failureRedirect: "/"
+  })
+);
 
-// ----- GET: logged (pug)
-app.get("/auth/google", function(req, res) {
-  const response = {
-    first_name: req.query.first_name,
-    last_name: req.query.last_name
-  };
-  console.log(response);
-  res.render("logged.pug", response);
-});
-
+// ----- server
 var server = app.listen(3000, "localhost", function() {
   var host = server.address().address;
   var port = server.address().port;
